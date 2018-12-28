d9448c5a36b0e2a1c70b1592e983516bdd7e6454
issuser <issuser@ISS>
2018-12-28 17:15:06 +0800

    添加keyvalueVo  删除TestVo    wxj20181228

---
+++ leishi-kernel-client/src/main/java/com/wxj/domain/vo/KeyValueVo.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.wxj.domain.vo;
+
+/**  
+* @ClassName: KeyValueVo  
+* @Description: TODO KeyValueVo  
+* @author: wxj  
+* @date: 2018年12月28日
+* @Tel:18772118541
+* @email:18772118541@163.com
+*/
+public class KeyValueVo {
+	private String keywords;
+	private String value;
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
--- leishi-kernel-client/src/main/java/com/wxj/domain/vo/TestVo.java
+++
@@ -1,5 +0,0 @@
-package com.wxj.domain.vo;
-
-public class TestVo {
-
-}
